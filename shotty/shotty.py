1b10e6901097a8181083804c82525612560831db
Vijay Janarthanan <vijayjanarthanan@yahoo.co.in>
2020-08-16 22:24:25 +0530

    Update script to use the click module and show more information about the instances

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,7 +1,20 @@
 import boto3
+import click
+
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
+
+@click.command()
+def listInstances():
+    "List of EC2 Instances"
+    for inst in ec2.instances.all():
+        print(', '.join((
+            inst.id,
+            inst.instance_type,
+            inst.placement['AvailabilityZone'],
+            inst.state['Name'],
+            inst.public_dns_name)))
+    return
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name='shotty')
-    ec2 = session.resource('ec2')
-    for inst in ec2.instances.all():
-        print(inst)
+    listInstances()
